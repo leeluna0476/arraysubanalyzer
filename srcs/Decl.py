78c1c98c40e00bc621893bbec1992d0d0c1e0a5d
LEE LUNA <seojilee@student.42seoul.kr>
2026-04-26 16:36:59 +0900

    apply changed registry class method name, add a new attribute 'initialized', and change get_id to get_by_id for clearer statement

--- srcs/Decl.py
+++ srcs/Decl.py
@@ -4,30 +4,35 @@ from Stmt import DeclStmt
 class Decl(Common):
     _kind = 'Decl'
 
-    def __init__(self, idata=None):
-        super().__init__(idata)
-
 class VarDecl(Decl):
     _kind = 'VarDecl'
     _registry = {}
 
     def __init__(self, idata=None):
         super().__init__(idata)
-        self._initialized = False
+        if 'init' in self._data:
+            self._initialized = True
+        else:
+            self._initialized = False
     
     # a c variable always has a name
     @property
     def name(self):
         return self._data['name']
 
+    @property
+    def initialized(self):
+        return self._initialized
+
     # create a VarDecl object with cid under DeclStmt
     # does not create the other objects unlike listup_obj
     @classmethod
-    def get_id(cls, cid):
+    def get_by_id(cls, cid):
         obj = cls.get_obj(cid)
         if not obj:
-            for v in DeclStmt._registry.values():
-                l = cls.listup_data(v.data)
+            declstmt_objs = DeclStmt.registry_view().values()
+            for o in declstmt_objs:
+                l = cls.listup_data(o.data)
                 for d in l:
                     if d['id'] == cid:
                         obj = cls(d)
