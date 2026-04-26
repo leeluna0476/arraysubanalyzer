a8f1ca4a2828454d989600c054d76d981393e6ee
LEE LUNA <seojilee@student.42seoul.kr>
2026-04-26 22:53:28 +0900

    apply the changes to the test script

--- srcs/test.py
+++ srcs/test.py
@@ -3,19 +3,18 @@ from Expr import ArraySubscriptExpr, ImplicitCastExpr, DeclRefExpr
 from Decl import VarDecl
 from Stmt import DeclStmt
 from asttools import filter_by_qualtype
+from ASTNode import ASTNode
 
 with open('jsondump', mode='r', encoding='utf-8') as f:
     s = f.read()
     data = json.loads(s)
+    ASTNode.register_rawdata(data)
 
-    # must define DeclStmt first in advance of VarDecl
-    decl_stmt_list = DeclStmt.listup_obj(data)
-#    var_list = VarDecl.listup_from_parentlist(decl_stmt_list)
-    ase_list = ArraySubscriptExpr.listup_obj(data)
-    ice_list = ImplicitCastExpr.listup_from_parentlist(ase_list)
-    dre_list = DeclRefExpr.listup_from_parentlist(ice_list)
+    ase_list = ArraySubscriptExpr.listup_obj()
+    dre_list = DeclRefExpr.listup_obj_under_parent(ase_list)
     var_list = DeclRefExpr.listup_referenced_decl(dre_list)
+    var_set = set(var_list)
     var_int_list = filter_by_qualtype(var_list, 'int')
 
-    for v in var_int_list:
-        print(f'id={id(v)}, name={v.name}')
+    for v in var_set:
+        print(f'id={id(v)}, name={v.name}, initialized={v.initialized}')
