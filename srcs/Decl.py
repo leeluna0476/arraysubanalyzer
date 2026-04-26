947ef1e15970912be0eb9b612d1b6ee7015451fd
LEE LUNA <seojilee@student.42seoul.kr>
2026-04-26 13:50:39 +0900

    add VarDecl.getid that creates a VarDecl object by cid under DeclStmt

--- srcs/Decl.py
+++ srcs/Decl.py
@@ -1,4 +1,5 @@
 from Common import Common
+from Stmt import DeclStmt
 
 class Decl(Common):
     _kind = 'Decl'
@@ -12,10 +13,25 @@ class VarDecl(Decl):
 
     def __init__(self, idata=None):
         super().__init__(idata)
-        print('Var init')
         self._initialized = False
     
     # a c variable always has a name
     @property
     def name(self):
         return self._data['name']
+
+    # create a VarDecl object with cid under DeclStmt
+    # does not create the other objects unlike listup_obj
+    @classmethod
+    def get_id(cls, cid):
+        obj = cls.get_obj(cid)
+        if not obj:
+            for v in DeclStmt._registry.values():
+                l = cls.listup_data(v.data)
+                for d in l:
+                    if d['id'] == cid:
+                        obj = cls(d)
+                        break
+                if obj:
+                    break
+        return obj
--- srcs/test.py
+++ srcs/test.py
@@ -1,12 +1,16 @@
 import json
 from Expr import ArraySubscriptExpr, ImplicitCastExpr, DeclRefExpr
 from Decl import VarDecl
+from Stmt import DeclStmt
 from asttools import filter_by_qualtype
 
 with open('jsondump', mode='r', encoding='utf-8') as f:
     s = f.read()
     data = json.loads(s)
 
+    # must define DeclStmt first in advance of VarDecl
+    decl_stmt_list = DeclStmt.listup_obj(data)
+#    var_list = VarDecl.listup_from_parentlist(decl_stmt_list)
     ase_list = ArraySubscriptExpr.listup_obj(data)
     ice_list = ImplicitCastExpr.listup_from_parentlist(ase_list)
     dre_list = DeclRefExpr.listup_from_parentlist(ice_list)
@@ -14,4 +18,4 @@ with open('jsondump', mode='r', encoding='utf-8') as f:
     var_int_list = filter_by_qualtype(var_list, 'int')
 
     for v in var_int_list:
-        print(v.name)
+        print(f'id={id(v)}, name={v.name}')
