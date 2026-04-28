cb2179a83304def8a51aa032fe7a50bf2fd796c9
LEE LUNA <seojilee@student.42seoul.kr>
2026-04-29 00:30:32 +0900

    add a line info to each node referencing its parent's

--- srcs/ASTNode.py
+++ srcs/ASTNode.py
@@ -13,14 +13,18 @@ class ASTNode:
         return MappingProxyType(cls._raw)
     
     @classmethod
-    def register_rawdata(cls, rawdata):
+    def register_rawdata(cls, rawdata, pline=0):
         if not cls._raw:
             cls._raw = rawdata
+        begin = rawdata.get('range', {}).get('begin', {})
+        if begin:
+            begin['line'] = max(begin.get('line', 0), pline)
+
         cid = rawdata['id']
         cls._nodes[cid] = rawdata
         if 'inner' in rawdata:
             for d in rawdata['inner']:
-                cls.register_rawdata(d)
+                cls.register_rawdata(d, begin.get('line', 0))
 
     @classmethod
     def get_node(cls, cid):
