29634c855bcd1d2840ec92e46ca4f6232ad30792
LEE LUNA <seojilee@student.42seoul.kr>
2026-04-28 21:36:18 +0900

    receive input from stdin

--- srcs/test.py
+++ srcs/test.py
@@ -1,3 +1,4 @@
+import sys
 import json
 from Expr import ArraySubscriptExpr, ImplicitCastExpr, DeclRefExpr
 from Decl import VarDecl
@@ -5,16 +6,15 @@ from Stmt import DeclStmt
 from asttools import filter_by_qualtype
 from ASTNode import ASTNode
 
-with open('jsondump', mode='r', encoding='utf-8') as f:
-    s = f.read()
-    data = json.loads(s)
-    ASTNode.register_rawdata(data)
+s = sys.stdin.read()
+data = json.loads(s)
+ASTNode.register_rawdata(data)
 
-    ase_list = ArraySubscriptExpr.listup_obj()
-    dre_list = DeclRefExpr.listup_obj_under_parent(ase_list)
-    var_list = DeclRefExpr.listup_referenced_decl(dre_list)
-    var_set = set(var_list)
-    var_int_list = filter_by_qualtype(var_list, 'int')
+ase_list = ArraySubscriptExpr.listup_obj()
+dre_list = DeclRefExpr.listup_obj_under_parent(ase_list)
+var_list = DeclRefExpr.listup_referenced_decl(dre_list)
+var_set = set(var_list)
+var_int_list = filter_by_qualtype(var_list, 'int')
 
-    for v in var_set:
-        print(f'id={id(v)}, name={v.name}, initialized={v.initialized}')
+for v in var_set:
+    print(f'id={id(v)}, name={v.name}, initialized={v.initialized}')
